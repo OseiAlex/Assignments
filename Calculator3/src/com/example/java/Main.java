98e538b751fd8ccf183d90408ce008eace0354fd
OseiAlex <34588600+OseiAlex@users.noreply.github.com>
2018-02-13 09:33:30 +0000

    Calculator assignment complete
    Calculator assignment complete

---
+++ Calculator3/src/com/example/java/Main.java
@@ -0,0 +1,15 @@
+package com.example.java;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Calculator calculate = new Calculator();
+
+        calculate.setFirstNumber(22);
+        calculate.setSecondNumber(7);
+        calculate.Addition();
+        calculate.Multiplication();
+        calculate.Division();
+
+    }
+}
